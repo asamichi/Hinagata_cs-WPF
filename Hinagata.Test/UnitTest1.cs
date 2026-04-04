22f23247ae39310f7ba950171df9013d7983d33d
asamichi <asamichi.dev@gmail.com>
2026-04-05 01:51:05 +0900

    [Add] 汎用処理として、JsonStorage を追加。JsonStorage 単体テストのため、xUnit によるテストプロジェクト、テスト処理を追加。

---
+++ Hinagata.Test/UnitTest1.cs
@@ -0,0 +1,103 @@
+﻿namespace Hinagata.Test;
+
+using Hinagata.Commons;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+public class JsonStorageTests
+{
+    [Fact]
+    public void SaveJson()
+    {
+        //準備
+        string filename = "Test.json";
+        if (File.Exists(filename)) {
+            File.Delete(filename);
+        }
+        JsonStorage<string, string> storage = new JsonStorage<string, string>(filename);
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        data.Add("host", "localhost");
+        data.Add("port", "3306");
+        data.Add("user", "admin");
+        data.Add("password", "pass");
+
+        //実行
+        storage.SaveJson(data);
+
+        //検証
+        JsonStorage<string, string> storage2 = new JsonStorage<string, string>(filename);
+        Dictionary<string, string> data2 = storage2.LoadJson();
+        //data["host"] = "192.168.7.7";
+        Assert.Equal(data, data2);
+        if (File.Exists(filename))
+        {
+            //テスト終了時に実体を見たいのでスキップ
+            //File.Delete(filename); 
+        }
+    }
+
+    public static IEnumerable<object[]> GetTestData()
+    {
+        List<object[]> data = new List<object[]>();
+
+        AddObjectToList(data, new Dictionary<string, string>()
+        {
+            {"host","lcalhost"},
+            { "port","3306"}
+        });
+
+        AddObjectToList(data, new Dictionary<int, int>()
+        {
+            {1,1 },
+            {-1,-1 }
+        });
+
+        AddObjectToList(data, new Dictionary<string, Dictionary<string, string>>()
+        {
+            {"user1",new Dictionary<string, string>(){{"type","Attack"}} }
+        });
+
+
+        foreach (var i in data)
+        {
+            yield return i;
+        }
+    }
+
+    //記述簡略化のためのメソッド
+    private static void AddObjectToList(List<object[]> list,object data)
+    {
+        list.Add(new object[] { data });
+    }
+
+
+
+    [Theory]
+    //[InlineData(new Dictionary<int, int>()
+    //{
+    //    {1,1 },
+    //    {-1,-1 }
+    //})]
+    //MemberData(nameof(メソッド)),3] のようにすると引数を渡せる
+    [MemberData(nameof(GetTestData))]
+    public void TestSaveJson<TKey,Tvalue>(Dictionary<TKey,Tvalue> data) where TKey: notnull
+    {
+        string filename = "Test2.json";
+        if (File.Exists(filename))
+        {
+            File.Delete(filename);
+        }
+        var storage = new JsonStorage<TKey, Tvalue>(filename);
+
+        //テストデータの保存
+        storage.SaveJson(data);
+
+        //検証
+        var storage2 = new JsonStorage<TKey, Tvalue>(filename);
+        var data2 = storage2.LoadJson();
+
+        Assert.Equal(data, data2);
+        
+    }
+}
---
+++ Hinagata/Commons/JsonStorage.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Hinagata.Commons;
+
+//ジェネリッククラス。where 以下は TKey が null を許容しないということ。
+public class JsonStorage<TKey, TValue> where TKey : notnull
+{
+    private readonly string _filePath;
+    //デフォルトの json を指定する場合使用
+    private readonly string _defaultJSONString;
+    //保存時のオプション
+    private readonly JsonSerializerOptions _options = new JsonSerializerOptions
+    {
+        WriteIndented = true, // これで改行とインデントが入る
+        Converters = { new JsonStringEnumConverter() } //これで Enum が文字列に変換して保存されるように
+    };
+
+    public JsonStorage(string fileName, string defaultJSONString = "{}")
+    {
+        //実行ファイル + fileName で json ファイルのパスを生成
+        _filePath = Path.Combine(AppContext.BaseDirectory, fileName);
+        _defaultJSONString = defaultJSONString;
+    }
+
+    public void SaveJson(Dictionary<TKey,TValue> data)
+    {
+        string json = JsonSerializer.Serialize(data, _options);
+        File.WriteAllText(_filePath, json);
+    }
+
+    public Dictionary<TKey, TValue> LoadJson()
+    {
+        //ファイルが無い場合にはデフォルト Json のディクショナリを返す
+        if (!File.Exists(_filePath))
+        {
+            return LoadDefaultJson();
+        }
+
+        try
+        {
+            string json = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<Dictionary<TKey, TValue>>(json) ?? LoadDefaultJson();
+        }
+        catch
+        {
+            //ファイルが壊れている場合等、一旦仮
+            return LoadDefaultJson();
+        }
+    }
+
+    
+    private Dictionary<TKey,TValue> LoadDefaultJson()
+    {
+        string defaultJson = _defaultJSONString;
+        return JsonSerializer.Deserialize<Dictionary<TKey, TValue>>(defaultJson) ?? new Dictionary<TKey, TValue>(); ;
+    }
+}
