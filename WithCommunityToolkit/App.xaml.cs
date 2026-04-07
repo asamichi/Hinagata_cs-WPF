113a252b4e69ac3d8993735a2e0825ce5c5f0bb3
asamichi <asamichi.dev@gmail.com>
2026-04-08 05:32:50 +0900

    [Updata] DI コンテナを導入

--- WithCommunityToolkit/App.xaml.cs
+++ WithCommunityToolkit/App.xaml.cs
@@ -7,6 +7,9 @@ using WithCommunityToolkit.Views;
 using WithCommunityToolkit.ViewModels;
 using WithCommunityToolkit.Models;
 
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace WithCommunityToolkit;
 
@@ -16,25 +19,49 @@ namespace WithCommunityToolkit;
 public partial class App : Application
 {
     protected override void OnStartup(StartupEventArgs e)
-    { 
-        //ストレージ
-        JsonStorage<string, int> storage = new JsonStorage<string, int>("Cnt.json", new Dictionary<string, int>() { { "Value", 0 } });
+    {
 
-        CntClass cntModel = new CntClass();
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder();
 
-        //vm
-        var cntViewModel = new CntViewModel(cntModel,storage);
-        var evenOddViewModel = new EvenOddViewModel(cntModel);
-        var cntWindowViewModel = new CntWindowViewModel(cntViewModel, evenOddViewModel);
+        //ストレージ登録
+        builder.Services.AddTransient<JsonStorage<string,int>>(sp =>
+        {
+            return new JsonStorage<string, int>("Cnt.json", new Dictionary<string, int>() { { "Value", 0 } });
+        });
 
-        //初期起動するウィンドウ
-        //var mainWindow = new MainWindow();
-        //mainWindow.DataContext = cntViewModel;
-        //mainWindow.Show();
+        //モデル登録
+        builder.Services.AddTransient<CntClass>();
+
+        //vm 登録
+        builder.Services.AddTransient<CntViewModel>();
+        builder.Services.AddTransient<EvenOddViewModel>();
+        builder.Services.AddTransient<CntWindowViewModel>();
+
+        IHost host = builder.Build();
 
         var cntWindow = new CntWindow();
-        cntWindow.DataContext = cntWindowViewModel;
+        cntWindow.DataContext = host.Services.GetRequiredService<CntWindowViewModel>();
         cntWindow.Show();
+
+
+        ////ストレージ
+        //JsonStorage<string, int> storage = new JsonStorage<string, int>("Cnt.json", new Dictionary<string, int>() { { "Value", 0 } });
+
+        //CntClass cntModel = new CntClass();
+
+        ////vm
+        //var cntViewModel = new CntViewModel(cntModel,storage);
+        //var evenOddViewModel = new EvenOddViewModel(cntModel);
+        //var cntWindowViewModel = new CntWindowViewModel(cntViewModel, evenOddViewModel);
+
+        ////初期起動するウィンドウ
+        ////var mainWindow = new MainWindow();
+        ////mainWindow.DataContext = cntViewModel;
+        ////mainWindow.Show();
+
+        //var cntWindow = new CntWindow();
+        //cntWindow.DataContext = cntWindowViewModel;
+        //cntWindow.Show();
 
     }
 
